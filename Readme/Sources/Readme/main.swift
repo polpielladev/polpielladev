aff837e4cc4eae7b4948e76799755cf3adfba403
Pol Piella <info@polpiellamusic.com>
2021-07-23 17:23:10 +0100

    Minor change to trigger the pipeline

--- Readme/Sources/Readme/main.swift
+++ Readme/Sources/Readme/main.swift
@@ -2,7 +2,7 @@ import Foundation
 import ArgumentParser
 
 struct Readme: ParsableCommand {
-    @Argument(help: "The template file that is to be used to parse and replace content.")
+    @Argument(help: "The template file will be used to parse and replace content.")
     var template: String
     
     @Argument(help: "The path of the destination README file.")
